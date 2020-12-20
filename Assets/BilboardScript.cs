aada24529b3dacf59d40e6523edd567cfdb3f392
Evgeny Shapiro <shapiro.evg@gmail.com>
2020-12-20 21:16:05 +0200

    After dead return to last spawn object

---
+++ Assets/BilboardScript.cs
@@ -0,0 +1,22 @@
+﻿using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BilboardScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+            if (model != null)
+            {
+                model.spawnPoint = transform;
+                Debug.Log("Update spawn model");
+            }
+        }
+    }
+}
