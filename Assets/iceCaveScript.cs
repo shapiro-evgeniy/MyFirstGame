15f0b03416c63bd106a8105314997efcb0f7f55a
Evgeny Shapiro <shapiro.evg@gmail.com>
2020-12-25 10:25:03 +0200

    Add stones for mailstone. Change color of the player

---
+++ Assets/iceCaveScript.cs
@@ -0,0 +1,19 @@
+﻿using Platformer.Core;
+using Platformer.Model;
+using UnityEngine;
+
+public class iceCaveScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+            if (model != null)
+            {
+                model.spawnPoint = transform;
+                Debug.Log("Update spawn model");
+            }
+        }
+    }
+}
