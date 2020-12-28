677aa8f4def517a151c275ea4159915a1ba928fc
Evgeny Shapiro <shapiro.evg@gmail.com>
2020-12-28 16:18:55 +0200

    Fixed pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
